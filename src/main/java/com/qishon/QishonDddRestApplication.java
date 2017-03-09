785ac3ad4b4bdc972efc4824826d2b6837cd3152
Hero <42113026@qq.com>
2017-03-09 13:57:45 +0800

    add project files

---
+++ src/main/java/com/qishon/QishonDddRestApplication.java
@@ -0,0 +1,12 @@
+package com.qishon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QishonDddRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QishonDddRestApplication.class, args);
+	}
+}
